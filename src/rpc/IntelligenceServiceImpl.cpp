81bb2f2bb70bddb0112c3723a5e9cd2d28b374f4
kamran saberifard <kamisaberi@gmail.com>
2026-09-21 04:04:21 +0330

    some bugs fixed

--- src/rpc/IntelligenceServiceImpl.cpp
+++ src/rpc/IntelligenceServiceImpl.cpp
@@ -6,23 +6,20 @@ namespace sentinel::nexus::rpc {
 
 grpc::Status IntelligenceServiceImpl::SyncCollectiveImmunity(
     grpc::ServerContext* context, 
-    grpc::ServerReaderWriter<intelligence::FleetDefenseRule, intelligence::ThreatIndicator>* stream) {
+    grpc::ServerReaderWriter<::sentinel::nexus::FleetDefenseRule, ::sentinel::nexus::ThreatIndicator>* stream) {
     (void)context;
 
-    intelligence::ThreatIndicator incoming_threat;
+    ::sentinel::nexus::ThreatIndicator incoming_threat;
     std::string assigned_node_id;
 
-    // Read initial frame to identify node
     if (stream->Read(&incoming_threat)) {
         assigned_node_id = incoming_threat.origin_node_id();
         intelligence::IocBroadcaster::instance().register_subscriber(assigned_node_id, stream);
         
-        // Broadcast the initial threat if it contains payload
         if (!incoming_threat.attacker_ip().empty()) {
             intelligence::IocBroadcaster::instance().broadcast_threat(incoming_threat);
         }
 
-        // Loop and stream continuously
         while (stream->Read(&incoming_threat)) {
             if (!incoming_threat.attacker_ip().empty()) {
                 intelligence::IocBroadcaster::instance().broadcast_threat(incoming_threat);
