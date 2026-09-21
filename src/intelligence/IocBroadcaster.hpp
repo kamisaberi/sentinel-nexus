7b676494b38bbeed5d9ba4878d0dda99a94f3f87
kamran saberifard <kamisaberi@gmail.com>
2026-09-21 04:04:47 +0330

    some bugs fixed

--- src/intelligence/IocBroadcaster.hpp
+++ src/intelligence/IocBroadcaster.hpp
@@ -3,7 +3,6 @@
 #include <unordered_map>
 #include <mutex>
 #include <memory>
-#include <queue>
 #include <grpcpp/grpcpp.h>
 #include "intelligence.grpc.pb.h"
 
@@ -16,15 +15,11 @@ public:
         return inst;
     }
 
-    using StreamContext = grpc::ServerReaderWriter<FleetDefenseRule, ThreatIndicator>;
+    using StreamContext = grpc::ServerReaderWriter<::sentinel::nexus::FleetDefenseRule, ::sentinel::nexus::ThreatIndicator>;
 
-    // Registers an appliance's live bidirectional intelligence stream
     void register_subscriber(const std::string& node_id, StreamContext* stream);
     void unregister_subscriber(const std::string& node_id);
-
-    // Broadcasts an attack fingerprint to every connected appliance in the fleet (< 50ms)
-    void broadcast_threat(const ThreatIndicator& threat, uint64_t ttl_seconds = 86400);
-
+    void broadcast_threat(const ::sentinel::nexus::ThreatIndicator& threat, uint64_t ttl_seconds = 86400);
     size_t subscriber_count() const;
 
 private:
